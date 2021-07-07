3bb33496a9a2d662f43327a7a94f24bb53e74c40
ArcanumLibella <fiona.roux@hetic.net>
2021-07-07 14:34:39 +0200

    feat: display results list

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,17 +1,44 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
 
 export default function Search() {
-    const [term, setTerm] = useState("")
+    const [term, setTerm] = useState("dragonfly")
+    const [results, setResults] = useState([])
 
-    // const onSearch = (e) => {
-    //     // setTerm(e.target.value)
-    //     console.log(e.target.value)
-    // }
+    const renderedResults = results.map(result => {
+        return (
+            <div key={result.pageid} className="my-2 p-4 rounded-sm bg-blue-50">
+                <div className="mb-2 text-md font-medium">
+                    {result.title}
+                </div>
+                <div className="text-sm">
+                    {result.snippet}
+                </div>
+            </div>
+        )
+    })
+
+    useEffect(() => {
+        const search = async () => {
+            const {data} = await axios.get('https://en.wikipedia.org/w/api.php', {
+                params: {
+                    action: 'query',
+                    list: 'search',
+                    origin: '*',
+                    format: 'json',
+                    srsearch: term
+                }
+            })
+            setResults(data.query.search)
+        }
+
+        search()
+    }, [term])
 
     return (
         <div>
             <h2 className="mb-4 text-xl font-bold uppercase">Wikipedia Search</h2>
-            <form>
+            <form className="mb-8">
                 <fieldset className="flex flex-col">
                     <label className="mb-2 text-sm font-bold">Enter search term</label>
                     <input 
@@ -23,6 +50,10 @@ export default function Search() {
                     />
                 </fieldset>
             </form>
+
+            <div className="">
+                {renderedResults}
+            </div>
         </div>
     )
 }
