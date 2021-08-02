53184431878ec243bd27329b3c8399c1f747d98c
ArcanumLibella <fiona.roux@hetic.net>
2021-08-02 17:02:03 +0200

    fix: responsive

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -5,17 +5,17 @@ export default function Header() {
     return (
         <header className="bg-rose-corail">
             <Link to="/">
-                <h1 className="p-4 text-2xl font-black text-center text-black">React Widget</h1>
+                <h1 className="p-4 md:text-2xl font-black text-center text-black">React Widget</h1>
             </Link>
             
-            <nav className="flex justify-center p-4 text-xl font-bold text-center bg-rose text-white">
-                <Link to="/accordion" className="mx-8">
+            <nav className="flex flex-col md:flex-row justify-center p-4 md:text-xl font-bold text-center bg-rose text-white">
+                <Link to="/accordion" className="mx-8 my-1 md:my-0">
                     Accordion
                 </Link>
-                <Link to="/search" className="mx-8">
+                <Link to="/search" className="mx-8 my-1 md:my-0">
                     Wikipedia
                 </Link>
-                <Link to="/dropdown" className="mx-8">
+                <Link to="/dropdown" className="mx-8 my-1 md:my-0">
                     Dropdown Color
                 </Link>
             </nav>
