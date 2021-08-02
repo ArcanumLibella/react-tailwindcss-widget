9c1700badd0e4e2110413ce5f783824d3657a240
ArcanumLibella <fiona.roux@hetic.net>
2021-08-02 16:57:27 +0200

    feat: add open logic dropdown

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { ChevronDownIcon } from '@heroicons/react/solid'
 
 export default function Dropdown({selected, onSelectedChange, options}) {
+    const [open, setOpen] = useState(false);
 
     const renderedOptions = options.map(option => {
         if (option.value === selected.value) {
@@ -11,20 +12,32 @@ export default function Dropdown({selected, onSelectedChange, options}) {
             <li 
                 key={option.value}
                 onClick={() => onSelectedChange(option)}
-                className={`flex justify-between items-center text-grey-dark p-4 font-medium hover:bg-grey-light hover:text-${option.value} cursor-pointer`}
+                className={`flex justify-between items-center text-grey-dark p-4 font-medium hover:bg-grey-light cursor-pointer
+                hover:text-${option.value} 
+                ${open ? 'hidden' : 'block'}
+                `}
             >
                 {option.label}
                 <div className={`h-6 w-6 rou rounded-md bg-${option.value}`}></div>
             </li>
         )
     })
+
+    const handleDropdown = () => {
+        setOpen(!open)
+        console.log(open)
+    }
+
     return (
         <form>
             <fieldset className="">
                 <label className="flex justify-between items-center p-4 pb-2 font-bold">
                     Select an option :
                 </label>
-                <ul className="dropdown-menu rounded-md border border-grey">
+                <ul 
+                    onClick={() => handleDropdown()}
+                    className="dropdown-menu rounded-md border border-grey"
+                >
                     <li className={`flex justify-between items-center p-4 font-medium cursor-pointer text-${selected.value} border border-grey`}>
                         {selected.label}
                         <div className="flex justify-between items-center">
